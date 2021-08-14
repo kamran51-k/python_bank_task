f48aafb49e9b0d0d1b617d32af625969c61d3bfc
kamran51-k <85585311+kamran51-k@users.noreply.github.com>
2021-08-14 12:06:31 +0400

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,16 +1,8 @@
 a = open('bank.txt','r')
 read_file = a.read()
 list1 = read_file.split(',')
-sum = 0
-while True:
-     k = """
-          1.Total
-          2.deposit number
-          3.withdraw number
-          4.Quit
-          """
-     print(k)
-     c = input('Choose: ')
+def bank():
+     sum = 0
      deposit_number = 0
      withdraw_number = 0
      for i in list1:
@@ -20,16 +12,25 @@ while True:
                deposit_number += i
           elif i < 0:
                withdraw_number += i
-     if c == '1':
-          print(sum)
-     elif c == '2':
-          print(deposit_number)
-     elif c == '3':
-          print(withdraw_number)   
-     elif c == '4':
-          break    
+     while True:
+          k = """
+               1.Total
+               2.deposit number
+               3.withdraw number
+               4.Quit
+               """
+          print(k)
+          index_c = input('Choose: ')
+          if index_c == '1':
+               print(sum)
+          elif index_c == '2':
+               print(deposit_number)
+          elif index_c == '3':
+               print(withdraw_number)
+          elif index_c == '4':
+               break    
+print(bank())          
 
-     
 
 
 
